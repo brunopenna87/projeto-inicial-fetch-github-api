10b82894daa97f76f936be0076a8b205eec90ef3
brunopenna87 <bcpennaecosta@gmail.com>
2025-08-29 12:02:10 +0100

    feat: projeto inicial

---
+++ projeto-inicial-fetch-github-api/src/scripts/services/repositories.js
@@ -0,0 +1,9 @@
+import { baseUrl, repositoriesQuantity } from '../variables.js'
+
+async function getRepositories(userName) {
+    const responde = await fetch(`${baseUrl}/${userName}/repos?per_page=${repositoriesQuantity}`)
+    return await responde.json()
+}
+
+
+export { getRepositories }
